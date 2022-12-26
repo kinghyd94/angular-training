b6f67ff39ece958d4f9513883420e9ff97b556af
Mustafa <kinghyd94@gmail.com>
2022-12-27 00:18:35 +0530

    employee crud operations

--- src/app/employee-crud/employee-crud.component.ts
+++ src/app/employee-crud/employee-crud.component.ts
@@ -9,9 +9,12 @@ import { EmployeeService } from '../service/employee.service';
 export class EmployeeCrudComponent implements OnInit {
   allEmployees: any;
   columnHeader: any;
+  id:any;
   name:any = '';
   salary: any = '';
   gender: any = ''
+  newDynamic: any = {};
+  hideButton: boolean = true;
 constructor(private employeeService:EmployeeService){}
 ngOnInit(): void {
   this.employeeService.getAllEmployees().subscribe((employees)=>{
@@ -23,10 +26,55 @@ ngOnInit(): void {
 }
 
 addEmployee(name: any, sal:any, gender:any){
-  this.employeeService.postEmployee(name, sal,gender);
+  this.newDynamic = {
+    name: name, 
+    salary:sal,
+    gender:gender
+  }
+  this.employeeService.postEmployee(this.newDynamic).subscribe((empResponse)=>{
+    this.employeeService.getAllEmployees().subscribe((employees)=>{
+      this.allEmployees = empResponse;
+      this.columnHeader = Object.keys(this.allEmployees[0]);
+      this.columnHeader.push('Action')
+    });
+  });
 }
 delEmployee(id:any){
-  this.employeeService.deleteEmployee(id)
+  this.employeeService.deleteEmployee(id).subscribe((response)=>{
+    this.employeeService.getAllEmployees().subscribe((employees)=>{
+      this.allEmployees = employees;
+      this.columnHeader = Object.keys(this.allEmployees[0]);
+      this.columnHeader.push('Action')
+    });
+  })
+}
+editEmployee(id:number,name: any, sal:any, gender:any) {
+    this.name = name;
+    this.salary = sal;
+    this.gender = gender;
+    this.id = id;
+    this.hideButton = false;
+}
+
+updateEmployee(name: any, sal:any, gender:any){
+  this.newDynamic = {
+    name: name, 
+    salary:sal,
+    gender:gender
+  }
+  this.employeeService.updateEmployee(this.id, this.newDynamic).subscribe((empResponse)=>{
+    this.employeeService.getAllEmployees().subscribe((employees)=>{
+      this.allEmployees = employees;
+      this.columnHeader = Object.keys(this.allEmployees[0]);
+      this.columnHeader.push('Action')
+    });
+  });
+  this.name = '';
+  this.salary = '';
+  this.gender = '';
+  this.id = '';
+  this.hideButton = true;
+
 }
 
 }
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -5,16 +5,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EmployeeService {
- private url = 'http://localhost:3000/employees';
+ private url = "http://localhost:3000/employees";
   constructor(private http: HttpClient) { }
 
   getAllEmployees(){
    return this.http.get(this.url);
   }
-  postEmployee(name: any, salary:any, gender:any){
-    this.http.post(name, salary, gender);
+  postEmployee(empObj:any){
+    return this.http.post(this.url, empObj);
   }
   deleteEmployee(id:any){
-    this.http.delete(this.url+id)
+    let finalUrl = this.url+"/"+id;
+   return this.http.delete(finalUrl);
+  }
+  updateEmployee(id: number,employeeObj: any){
+    return this.http.patch(this.url+"/"+id,employeeObj);
   }
 }
